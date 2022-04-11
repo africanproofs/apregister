f133354e0a78bb111516b82f2ab8319e420753fe
Khosi Morafo <khosimorafo@yahoo.com>
2022-04-11 22:27:43 +0200

    chore: maintenance

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,24 +1,27 @@
 from brownie import accounts, FlareParticipantRegister, network, config
 
+
 def deploy_flare_participant_register():
     account = get_accounts()
-    register =  FlareParticipantRegister.deploy({"from": account})
-    
-    participants = register.getParticipants()
+    register = FlareParticipantRegister.deploy({"from": account})
 
-    print(f"The participant are {participants}")
+    # p = register.getParticipant('0x66aB6D9362d4F35596279692F0251Db635165871')
+
+    participants = register.getAllParticipants()
+
+    print(f"The number of participant is {len(participants)}")
 
     tx = register.register("African Proofs", "https://proofs.africa/participant.json")
 
-    participants = register.getParticipants()
+    participants = register.getAllParticipants()
 
-    print(f"The participans at are {participants}")
+    print(f"The number of participant is {len(participants)}")
 
     participant = register.getParticipant(participants[0])
 
     print(f"The first participant is {participant}")
 
-    tx = register.register("African Proofs", "https://www.proofs.africa/participant.json")
+    tx = register.register("African Proofs2", "https://www.proofs.africa/participant.json2")
 
     # participant = register.getParticipant(participants[0])
 
@@ -32,10 +35,6 @@ def deploy_flare_participant_register():
 
     print(f"The unregistered participant is {unregistered}")
 
-
-
-
-
     """ prvdrs = register.getProviders()
 
     print(prvdrs)
@@ -45,11 +44,13 @@ def deploy_flare_participant_register():
 
     print(prvdrs) """
 
+
 def get_accounts():
     if network.show_active() == "development":
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
 
 def main():
     deploy_flare_participant_register()
